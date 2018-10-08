e577f5acee83116587c01128ea4bc0580665cd89
dustinpfister <dustin.pfister@gmail.com>
2018-10-08 16:14:18 -0400

    digicur.js

---
+++ public/js/dig_cur_sim/digicur.js
@@ -0,0 +1,67 @@
+
+// Digital Currency Class
+var DigiCur = function (opt) {
+
+    opt = opt || {};
+
+    this.name = opt.name || 'floppyCoint';
+    this.baseRate = opt.baseRate || 0.001;
+    this.maxRate = opt.maxRate || 0.25;
+    this.maxHistory = opt.maxHistory || 10;
+    this.tickNum = 0;
+    this.history = [];
+
+    this.tick(this.maxHistory);
+
+};
+
+// tick the currency
+DigiCur.prototype.tick = function (count) {
+
+    var len;
+
+    count = count || 1;
+    while (count--) {
+
+        // push ne history object
+        this.history.push({
+            tickNum: this.tickNum,
+            rate: this.baseRate + Math.random() * (this.maxRate - this.baseRate)
+        });
+
+        // history length
+        len = this.history.length;
+
+        // step tick number
+        this.tickNum += 1;
+
+    }
+
+    // purge old history
+    if (len > this.maxHistory) {
+        this.history = this.history.splice(len - this.maxHistory, this.maxHistory);
+    }
+
+};
+
+// draw to the given phaser graphics object
+DigiCur.prototype.drawToGfx = function (gfx) {
+
+    gfx.clear();
+    gfx.lineStyle(3, 0x00ff00);
+
+    var points = [],
+    digi = this;
+
+    this.history.forEach(function (tick, i) {
+
+        var x = 10 + i * (120 / digi.maxHistory),
+        y = 120 - (tick.rate / digi.maxRate) * 120;
+
+        points.push(x, y);
+
+    })
+
+    gfx.drawPolygon(points);
+
+};
--- public/js/dig_cur_sim/main.js
+++ public/js/dig_cur_sim/main.js
@@ -1,70 +1,4 @@
 
-// Digital Currency Class
-var DigiCur = function (opt) {
-
-    opt = opt || {};
-
-    this.name = opt.name || 'floppyCoint';
-    this.baseRate = opt.baseRate || 0.01;
-    this.maxRate = opt.maxRate || 0.25;
-    this.maxHistory = opt.maxHistory || 10;
-    this.tickNum = 0;
-    this.history = [];
-
-    this.tick(this.maxHistory);
-
-};
-
-// tick the currency
-DigiCur.prototype.tick = function (count) {
-
-    var len;
-
-    count = count || 1;
-    while (count--) {
-
-        // push ne history object
-        this.history.push({
-            tickNum: this.tickNum,
-            rate: this.baseRate + Math.random() * (this.maxRate - this.baseRate)
-        });
-
-        // history length
-        len = this.history.length;
-
-        // step tick number
-        this.tickNum += 1;
-
-    }
-
-    // purge old history
-    if (len > this.maxHistory) {
-        this.history = this.history.splice(len - this.maxHistory, this.maxHistory);
-    }
-
-};
-
-// draw to the given phaser graphics object
-DigiCur.prototype.drawToGfx = function (gfx) {
-
-    gfx.clear();
-    gfx.lineStyle(3, 0x00ff00);
-
-    var points = [],
-    digi = this;
-
-    this.history.forEach(function (tick, i) {
-
-        var x = 10 + i * (120 / digi.maxHistory),
-        y = 120 - (tick.rate / digi.maxRate) * 120;
-
-        points.push(x, y);
-
-    })
-
-    gfx.drawPolygon(points);
-
-};
 
 // Create Graph
 var createGraph = function (game) {
@@ -83,7 +17,18 @@ game.state.add('demo', {
 
     create: function () {
 
-        game.data = game.data || {};
+        game.data = game.data || {
+
+            dollars: 0,
+            portfolio: {
+                dogeCoin: [{
+                        count: 10000,
+                        rate: 0.001
+                    }
+                ]
+
+            }
+        };
 
         game.data.dc = new DigiCur({
                 name: 'dogeCoin',
@@ -96,14 +41,14 @@ game.state.add('demo', {
 
         game.data.dc.drawToGfx(game.data.gfx);
 
-        console.log(game.data.dc.history);
-
     },
 
     update: function () {
 
-        game.data.dc.tick();
-        game.data.dc.drawToGfx(game.data.gfx);
+        var dc = game.data.dc;
+
+        dc.tick();
+        dc.drawToGfx(game.data.gfx);
 
     }
 
